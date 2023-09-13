2b7f1c3035c902fe18d7e9b483b651bd1b55101a
Devansh Kaneriya <devansh.kaneriya@kevit.io>
2023-09-13 18:01:49 +0530

    Chapter 8

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,33 @@
-console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageone = document.querySelector('#message-1')
+const messagetwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageone.textContent = 'Loading...'
+    messagetwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.error) {
+                messageone.textContent = data.error;
+            } else {
+                messageone.textContent = data.location;
+                messagetwo.textContent = data.forecast;
+            }
+        })
+        .catch((error) => {
+            messageone.textContent = 'Error: ' + error.message;
+        });
+
+})
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,13 @@
 const request = require('request')
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+today = yyyy+'-'+mm+'-'+dd;
+// console.log(today);
 
 const geocode = (address, callback) => {
-    const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+ address +'/2023-09-12?unitGroup=us&key=HZFNHHVVGNTLW7Z5S946TVEQC'
+    const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+ address +'/'+today+'?unitGroup=us&key=HZFNHHVVGNTLW7Z5S946TVEQC'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
